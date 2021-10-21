5d8fafaf000b3cf0341adf51455d5bffab0d8788
Ryan <ryanabusa8134@gmail.com>
2021-10-20 22:43:03 -0400

    Some personal adjustments

--- Assets/Scripts/ParavoidUI/Fader.cs
+++ Assets/Scripts/ParavoidUI/Fader.cs
@@ -5,17 +5,31 @@ using UnityEngine.UI;
 
 namespace ParavoidUI
 {
-   public class Fader : MonoBehaviour
+    public class Fader : MonoBehaviour
     {
         //private Image screen;
         private bool isFaded;
+        public Button returnToMenu_button;
+        public Button back;
 
-        public KeyCode ToggleMenuPanel = KeyCode.Escape;
+        public KeyCode toggleMenuPanelKey = KeyCode.Escape;
 
         public void Awake()
         {
             //screen = GetComponent<Image>();
             UnFadeScreen();
+            returnToMenu_button.onClick.AddListener(delegate
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 1;
+                SceneLoader.LoadScene("TitleScene");
+            });
+
+            back.onClick.AddListener(delegate
+            {
+                ToggleMenuPanel();
+            });
         }
 
         public void Update()
@@ -25,27 +39,37 @@ namespace ParavoidUI
 
         public void UpdateGeneralInput()
         {
-            if (Input.GetKeyDown(ToggleMenuPanel))
+            if (Input.GetKeyDown(toggleMenuPanelKey))
             {
-                foreach (Transform child in transform)
-                {
-                    if (child.gameObject.name == "MenuPanelV2")
-                    {
-                        if (child.gameObject.activeInHierarchy)
-                        {
-                            Debug.Log("Deactive: " + child.gameObject.name);
-                            child.gameObject.SetActive(false);
-                        }
-                        else
-                        {
-                            Debug.Log("Active: " + child.gameObject.name);
-                            child.gameObject.SetActive(true);
-                        }
+                ToggleMenuPanel();
+            }
+        }
 
-                        break;
+        public void ToggleMenuPanel()
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.name == "MenuPanelV2")
+                {
+                    if (child.gameObject.activeInHierarchy)
+                    {
+                        Debug.Log("Deactive: " + child.gameObject.name);
+                        child.gameObject.SetActive(false);
+                        Cursor.lockState = CursorLockMode.Confined;
+                        Cursor.visible = false;
+                        Time.timeScale = 1;
                     }
+                    else
+                    {
+                        Debug.Log("Active: " + child.gameObject.name);
+                        child.gameObject.SetActive(true);
+                        Cursor.lockState = CursorLockMode.None;
+                        Cursor.visible = true;
+                        Time.timeScale = 0;
+                    }
+
+                    break;
                 }
-               
             }
         }
 
@@ -66,14 +90,14 @@ namespace ParavoidUI
 
         public void ToggleFade()
         {
-            if(!isFaded)
+            if (!isFaded)
             {
-                isFaded = false; 
+                isFaded = false;
                 UnFadeScreen();
-            } 
+            }
             else
                 FadeScreenTo(0.4f);
         }
-    } 
+    }
 }
 
